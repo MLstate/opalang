32fa369ad671a8d1eefdb88e2a3250b411131cc3
Grégoire Duchêne <gregoire.duchene@mlstate.com>
2011-08-23 11:51:30 +0200

    [feature] stdlib: Added new beta Socket module.
    This module is intended to provide some low-level networking if you
    don't want the burden that comes with Hlnet.
    
    Please be aware that this module is still experimental.

---
+++ opabsl/mlbsl/bslSocket.ml
@@ -0,0 +1,52 @@
+(*
+    Copyright © 2011 MLstate
+
+    This file is part of OPA.
+
+    OPA is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Affero General Public License, version 3, as
+    published by the Free Software Foundation.
+
+    OPA is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+    License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with OPA. If not, see <http://www.gnu.org/licenses/>.
+*)
+
+module C = QmlCpsServerLib
+open C.Ops
+
+##extern-type Socket.connection = Scheduler.connection_info
+
+let private_connect ?(secure_mode = Network.Unsecured) (addr: string) port
+                    (cont: Scheduler.connection_info -> unit) =
+  let inet_addr = Network.inet_addr_of_name addr in
+  let port_spec = Network.make_port_spec ~socket_type:Network.TCP
+                                         ~protocol:"raw" inet_addr port in
+  Network.connect Scheduler.default port_spec secure_mode cont
+
+##register [cps-bypass] connect: string, int,\
+                                 continuation(Socket.connection) -> void
+let connect addr port cont = private_connect addr port (fun x -> x |> cont)
+
+##register [cps-bypass] secure_connect: string, int, SSL.secure_type,\
+                                        continuation(Socket.connection) -> void
+let secure_connect addr port secure_type cont =
+  private_connect ~secure_mode:(Network.Secured secure_type) addr port
+                  (fun x -> x |> cont)
+
+##register close: Socket.connection -> void
+let close connection_info =
+  Scheduler.remove_connection Scheduler.default connection_info
+
+##register [cps-bypass] write: Socket.connection, string,\
+                               continuation(int) -> void
+let write connection_info data k =
+  Scheduler.write Scheduler.default connection_info data (fun i -> i |> k)
+
+##register [cps-bypass] read : Socket.connection, continuation(string) -> void
+let read connection_info k =
+  Scheduler.read Scheduler.default connection_info (fun (_, str) -> str |> k)
---
+++ stdlib/io/socket/socket.opa
@@ -0,0 +1,68 @@
+/*
+    Copyright © 2011 MLstate
+
+    This file is part of OPA.
+
+    OPA is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Affero General Public License, version 3, as
+    published by the Free Software Foundation.
+
+    OPA is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+    Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public
+    License along with OPA.  If not, see <http://www.gnu.org/licenses/>.
+*/
+type Socket.connection = external
+
+/**
+    {1 About this module}
+
+    ``Low-level'' network module.
+
+    This module helps you to do some low-level networking. You can
+    connect to a server, and write and read raw data to/from it.
+
+    {b Warning:} This module is still experimental.
+*/
+Socket = {{
+    /**
+     * Connect to a server without any encryption.
+     *
+     * {b Usage:} [connect(hostname, portnumber)] will connect to
+     * [hostname] on [portnumber].
+     *
+     * @return A connection.
+     */
+    connect: string, int -> Socket.connection = %%BslSocket.connect%%
+
+    /**
+     * Connect to a server over an SSL encryption.
+     *
+     * {b Usage:} [secure_connect(hostname, portnumber, secure_type)]
+     * will connect to [hostname] on [portnumber] using [secure_type].
+     *
+     * @return A connection.
+     */
+    secure_connect: string, int, SSL.secure_type -> Socket.connection =
+        %%BslSocket.secure_connect%%
+
+    /** Close the connection. */
+    close: Socket.connection -> void = %%BslSocket.close%%
+
+    /**
+     * Write data to a connection.
+     *
+     * @return The amount of bytes read.
+     */
+    write: Socket.connection, string -> int = %%BslSocket.write%%
+
+    /**
+     * Read data from a connection.
+     *
+     * @return The data read.
+     */
+    read: Socket.connection -> string = %%BslSocket.read%%
+}}
