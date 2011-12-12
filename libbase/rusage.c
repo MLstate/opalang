e4d51c4026b6070de055169da35a506570b78bc4
Frederic Ye <frederic.ye@mlstate.com>
2011-12-12 12:59:55 +0100

    [enhance] rusage: better way to compute memory usage on mac + added linux support

--- libbase/rusage.c
+++ libbase/rusage.c
@@ -15,17 +15,53 @@
     You should have received a copy of the GNU Affero General Public License
     along with OPA. If not, see <http://www.gnu.org/licenses/>.
 */
-#include <sys/resource.h>
-
-long usage() {
-  int who = RUSAGE_SELF;
-  struct rusage usage;
-  struct rusage *p = &usage;
-  getrusage(who, p);
-  return p->ru_maxrss;
-}
 
 #include <caml/memory.h>
+
+#if (defined(__APPLE__) && defined(__MACH__)) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__) /* MAC */
+
+#include <mach/task.h>
+#include <mach/mach.h>
+
+// http://blog.kuriositaet.de/?p=257
+int get_mem (unsigned int *rss)
+{
+    struct task_basic_info t_info;
+    mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+    if (KERN_SUCCESS != task_info(mach_task_self(),
+       TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count))
+    {
+        return -1;
+    }
+    *rss = t_info.resident_size;
+    return 0;
+}
+
+#else /* not MAC */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int get_mem (unsigned int *rss)
+{
+    FILE* f = fopen("/proc/self/statm", "r");
+    unsigned int res = -1;
+    long page_size = getpagesize();
+    if (!f) return -1;
+    if (fscanf(f, "%d\n", &res) != 1) return -1;
+    *rss = res*page_size;
+    fclose(f);
+    return 0;
+}
+
+#endif
+
+long usage() {
+  unsigned int rss = -1;
+  get_mem(&rss);
+  return rss;
+}
 
 value get_memory_usage() {
   CAMLparam0();
