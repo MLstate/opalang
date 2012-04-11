420d1ce2898400423b14a4b64162e7d7279f308a
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2012-04-11 11:48:39 +0200

    [fix] compiler, database, mongo: Updating of sum path

--- opa/pass_MongoAccessGeneration.ml
+++ opa/pass_MongoAccessGeneration.ml
@@ -498,17 +498,14 @@ module Generator = struct
                 let annotmap, uexpr =
                   if ty_is_sum gamma dataty then (
                     let annotmap, uexpr = update_to_expr ~set:false gamma annotmap u in
-                    match u with
-                    | DbAst.UExpr _ ->
-                        (* Special case for upsert without '$' modifier, needs
-                           _id to the update query. *)
-                        let _id =
-                          (Format.sprintf "/%a"
-                             (Format.pp_list "/" Format.pp_print_string) strpath)
-                        in
-                        let annotmap, _id = C.string annotmap _id in
-                        add_to_document gamma annotmap "_id" _id uexpr
-                    | _ -> annotmap, uexpr
+                    (* Special case for upsert without '$' modifier, needs
+                       _id to the update query. *)
+                    let _id =
+                      (Format.sprintf "/%a"
+                         (Format.pp_list "/" Format.pp_print_string) strpath)
+                    in
+                    let annotmap, _id = C.string annotmap _id in
+                    add_to_document gamma annotmap "_id" _id uexpr
                   ) else (
                     let u =
                       if ty_is_const gamma dataty then DbAst.UFlds [["value"], u]
