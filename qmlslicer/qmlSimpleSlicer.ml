MLstate/opalang

commit 3dd938c1fe0751b214b9b0b893b4017a21e08f84

[fix] slicer,both,protected: both value can use a protected value if it is also exposed (as before)

qmlslicer/qmlSimpleSlicer.ml
@@ -842,14 +842,15 @@ let check_side ~emit_error ~emit node =
         ~tagged:"server" ~use:"protected" Private_path
         "The directive will be ignored.";
     | Some {wish=Force; side=(Client|Both) as side} ->
-      if emit || emit_error then (
+      let c1 = side=Client || (match node.privacy with Published _ -> true | _ -> false) in
+      if not(c1) && (emit || emit_error) then (
       OManager.serror "@[<v>%a@]@\n@[<4>  %s is tagged as '%s' but it uses 'protected' values:@\n%a@]"
         pp_pos node
         (Ident.original_name node.ident)
         (side_str side)
         (pp_private_path pp_pos) node;
-        false
-      ) else true
+        c1
+      ) else c1
     | _ -> true
   ) else true
   in

commit 1381c032bbe81f4baf2ded806f8cf6cf68adb2d5

[fix] slicer,client,protected: fix a security issue introduced by 3dd938c1fe, refuses again value tagged client and protected

qmlslicer/qmlSimpleSlicer.ml
@@ -842,7 +842,7 @@ let check_side ~emit_error ~emit node =
         ~tagged:"server" ~use:"protected" Private_path
         "The directive will be ignored.";
     | Some {wish=Force; side=(Client|Both) as side} ->
-      let c1 = side=Client || (match node.privacy with Published _ -> true | _ -> false) in
+      let c1 = side=Both && (match node.privacy with Published _ -> true | _ -> false) in
       if not(c1) && (emit || emit_error) then (
       OManager.serror "@[<v>%a@]@\n@[<4>  %s is tagged as '%s' but it uses 'protected' values:@\n%a@]"
         pp_pos node