64058ec73e0658bcd649dbf3bc6943e47dbc305c
Frederic Ye <frederic.ye@mlstate.com>
2012-06-25 17:26:30 +0200

    [fix] make_package: no more moreopts

--- make_package.sh
+++ make_package.sh
@@ -260,7 +260,7 @@ if [ "$PKG" = "true" ]; then
     # if [ $NOOCAML = "true" ]; then
     # 	 MOREOPTS="--filter lib/opa/static/*"
     # fi
-    $PACK_MAN/Contents/MacOS/PackageMaker --root $INSTALLDIR --resources $OPAGENERAL/installer/Mac/Resources/ --scripts $OPAGENERAL/installer/Mac/Scripts --info $OPAGENERAL/installer/Mac/Info.plist --id com.mlstate.opa.pkg -o "$MYDIR/$PKG_NAME.pkg" -n $BUILDNUM --domain system --root-volume-only --discard-forks -m --verbose --title "Opa $VERSION_MAJOR" $MOREOPTS
+    $PACK_MAN/Contents/MacOS/PackageMaker --root $INSTALLDIR --resources $OPAGENERAL/installer/Mac/Resources/ --scripts $OPAGENERAL/installer/Mac/Scripts --info $OPAGENERAL/installer/Mac/Info.plist --id com.mlstate.opa.pkg -o "$MYDIR/$PKG_NAME.pkg" -n $BUILDNUM --domain system --root-volume-only --discard-forks -m --verbose --title "Opa $VERSION_MAJOR"
     echo "Creating image '$MYDIR/$PKG_NAME.dmg'"
     if [ -f "$MYDIR/$PKG_NAME.dmg" ]; then
 	rm "$MYDIR/$PKG_NAME.dmg"
