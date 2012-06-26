25f06327d03347a5b973104b15a64b927ac14aa6
Frederic Ye <frederic.ye@mlstate.com>
2012-06-26 13:03:00 +0200

    [enhance] build: remove deprecated warning on Mac

--- build/myocamlbuild_prefix.ml
+++ build/myocamlbuild_prefix.ml
@@ -39,7 +39,7 @@ let windows_mode = (os = Win32)
 
 let c_wall,c_werror =
   if windows_mode (*&& compiler=microsoft*) then "/Wall","/Wall"
-  else if is_mac then "-Wall","-Wall"
+  else if is_mac then "-Wall -Wdeprecated-declarations","-Wall"
   else "-Wall","-Werror"
 
 let winocamldir   = Pathname.pwd /  "ms_windows"
