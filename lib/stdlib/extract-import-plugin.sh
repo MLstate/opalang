5fa04a64c568385abdf4f1e5c45300967d6e451e
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2012-07-10 11:24:32 +0200

    [fix] build: Fix the platform_helper path

--- lib/stdlib/extract-import-plugin.sh
+++ lib/stdlib/extract-import-plugin.sh
@@ -2,6 +2,6 @@
 # script used to extract import plugin line
 
 # source platform_helper (for sed)
-. ${OPA_SOURCE_DIR:-$(dirname $0)/../../tools/}/platform_helper.sh
+. ${OPA_SOURCE_DIR:-$(dirname $0)/../../}/tools/platform_helper.sh
 
 sed -n "s%^ *import-plugin  *\\(.*\\) *$%\\1%p" $1 | sed -e "s/{//" -e "s/}//" -e "s/, */\n/g"
