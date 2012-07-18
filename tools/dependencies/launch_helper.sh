1f7c291f59cc7f10d38f2b898f17ef9c2ac8dd90
Bourgerie Quentin <bourgerie.quentin@gmail.com>
2012-07-18 16:53:28 +0200

    [fix] launch_script: I don't want npm stderr

--- tools/dependencies/launch_helper.sh
+++ tools/dependencies/launch_helper.sh
@@ -14,7 +14,7 @@ function check-node-dependencies() (
     MISSING=""
     for i in $1; do
 	# only keep first level modules
-        if ! npm list -parseable | sed -e '\#\(.*\)/node_modules/\(.*\)/node_modules/\(.*\)#{d}' | grep -q "$i" && ! npm list -g -parseable | sed -e '\#\(.*\)/node_modules/\(.*\)/node_modules/\(.*\)#{d}' | grep -q "$i"; then
+        if ! npm list -parseable 2> /dev/null | sed -e '\#\(.*\)/node_modules/\(.*\)/node_modules/\(.*\)#{d}' | grep -q "$i" && ! npm list -g -parseable | sed -e '\#\(.*\)/node_modules/\(.*\)/node_modules/\(.*\)#{d}' | grep -q "$i"; then
             MISSING="$MISSING $i"
         fi
     done
