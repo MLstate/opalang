ffaafd3d0fdf80d9c062caaa78f63d4e4c659038
Frederic Ye <frederic.ye@mlstate.com>
2012-07-19 18:25:47 +0200

    [enhance] build_rules: removed a useless include

--- tools/build/build_rules.ml
+++ tools/build/build_rules.ml
@@ -972,7 +972,6 @@ let package_building ?(nodebackend=false) ~name ~stamp ~stdlib_only ~rebuild () 
        let extra_opt =
          [A"--opx-dir";A opx_dir;
           A"--package-version"; A version;
-          A"-I";A opa_prefix;
           A"--no-warn";A"load-import";
 	  A"--no-warn";A"bsl.loading";
           A"--no-warn-error";A"unused";
