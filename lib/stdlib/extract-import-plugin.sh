55549baa127ceb089d1cd9fe3778bc729cf623dc
Jeremy Messenger <mezz@FreeBSD.org>
2012-08-04 17:38:44 -0500

    [fix] lib/stdlib/extract-import-plugin.sh: Replace the sed(1) with /n to tr(1).

--- lib/stdlib/extract-import-plugin.sh
+++ lib/stdlib/extract-import-plugin.sh
@@ -4,4 +4,4 @@
 # source platform_helper (for sed)
 . ${OPA_SOURCE_DIR:-$(dirname $0)/../../}/tools/platform_helper.sh
 
-sed -n "s%^ *import-plugin  *\\(.*\\) *$%\\1%p" $1 | sed -e "s/{//" -e "s/}//" -e "s/, */\n/g"
+sed -n "s%^ *import-plugin  *\\(.*\\) *$%\\1%p" $1 | sed -e "s/{//" -e "s/}//" -e "s/, /,/g" | tr ',' '\n'
