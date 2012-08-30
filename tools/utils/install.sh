3c917583a97b4d8b67d38550e3e317e5f53d44be
Frederic Ye <frederic.ye@mlstate.com>
2012-08-30 11:42:08 +0200

    [cleanup] install.sh: also uninstall opa-create

--- tools/utils/install.sh
+++ tools/utils/install.sh
@@ -165,3 +165,9 @@ create_wrapper $PREFIX/lib/opa/bin/opa-plugin-builder-bin opa-plugin-builder
 
 # creating wrapper script 'opa-plugin-browser'
 create_wrapper $PREFIX/lib/opa/bin/opa-plugin-browser-bin opa-plugin-browser
+
+if [ $UNINSTALL = true ]; then
+    opa_create=$INSTALLDIR/bin/opa-create
+    [ $QUIET = true ] || echo "Removing $opa_create"
+    rm -f $opa_create
+fi
