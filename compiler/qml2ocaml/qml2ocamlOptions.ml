df213e6b11f1dbaa15f56e293284a0f8f36ee0fc
Frederic Ye <frederic@opalang.org>
2012-10-18 19:10:17 +0200

    [cleanup] removed libirc, irc plugin and irc stdlib

--- compiler/qml2ocaml/qml2ocamlOptions.ml
+++ compiler/qml2ocaml/qml2ocamlOptions.ml
@@ -1,5 +1,5 @@
 (*
-    Copyright © 2011 MLstate
+    Copyright © 2011, 2012 MLstate
 
     This file is part of Opa.
 
@@ -105,7 +105,6 @@ struct
     "libtools" ;
     "libsecurity" ;
     "libnet" ;
-    "libirc" ;
     "database" ;
     "oparuntime";
     "libsession" ;
